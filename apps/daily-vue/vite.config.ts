5ab7bf1b0865b6ecbb76d03271ebd354eeb215fe
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-08-19 10:33:37 -0500

    chore(config): deletes Vue Daily UI

--- apps/daily-vue/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-export default defineConfig({
-  plugins: [tailwindcss(), vue(), tsconfigPaths()],
-});
